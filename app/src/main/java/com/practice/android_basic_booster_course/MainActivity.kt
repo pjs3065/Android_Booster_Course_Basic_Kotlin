5960afb620c8d084f0633124d9c22b6089585af9
pjs3065 <tmakdlfwotjd@naver.com>
2020-01-05 17:19:50 +0900

    시작

---
+++ app/src/main/java/com/practice/android_basic_booster_course/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.practice.android_basic_booster_course
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
