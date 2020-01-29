95a932d6a613d2c6685b88d292ca3f43846a5dbd
pjs3065 <tmakdlfwotjd@naver.com>
2020-01-29 18:36:50 +0900

    리스트형식의 다이얼로그 띄우기

--- DemoDialog/app/src/main/java/com/practice/demodialog/MainActivity.kt
+++ DemoDialog/app/src/main/java/com/practice/demodialog/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.practice.demodialog
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,21 +18,49 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
+//        showAlertDialogBasic()
+//        showAlertDialogSameBasic()
+//        showAlertDialogList()
+
+    }
+
+    /**
+     * 기본 다이얼로그 띄우기
+     */
+    private fun showAlertDialogBasic() {
         AlertDialog.Builder(this)
             .setIcon(R.drawable.ic_launcher_foreground)
             .setTitle(getString(R.string.title))
             .setMessage(getString(R.string.message))
             .setNeutralButton(getString(R.string.exit), null)
             .show()
+    }
 
+    /**
+     * 빌더패턴 안쓰고 기본 다이얼로그 띄우기
+     */
+    private fun showAlertDialogSameBasic() {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setIcon(R.drawable.ic_launcher_foreground)
+        dialog.setTitle(getString(R.string.title))
+        dialog.setMessage(getString(R.string.message))
+        dialog.setNeutralButton(getString(R.string.exit), null)
+        dialog.show()
+    }
 
-//        //같은 코드
-//        val dialog = AlertDialog.Builder(this)
-//        dialog.setIcon(R.drawable.ic_launcher_foreground)
-//        dialog.setTitle(getString(R.string.title))
-//        dialog.setMessage(getString(R.string.message))
-//        dialog.setNeutralButton(getString(R.string.exit), null)
-//        dialog.show()
+    /**
+     * 리스트 형식의 다이얼로그 띄우기
+     */
+    private fun showAlertDialogList() {
+        val cars = arrayOf("SM3", "SM5", "SM7", "SONATA", "AVANTE")
+
+        AlertDialog.Builder(this)
+            .setItems(cars, object : DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    Toast.makeText(this@MainActivity, "cars: ${cars[which]}", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }).setNeutralButton(getString(R.string.exit), null).show()
     }
 }
 
