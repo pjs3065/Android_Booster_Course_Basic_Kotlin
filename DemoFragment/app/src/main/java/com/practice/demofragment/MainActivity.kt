7da6bcd1a8879a1481a5524205f3ea5cec7c4db4
pjs3065 <tmakdlfwotjd@naver.com>
2020-02-03 12:45:51 +0900

    프래그먼트 자바 코드로 추가하기

--- DemoFragment/app/src/main/java/com/practice/demofragment/MainActivity.kt
+++ DemoFragment/app/src/main/java/com/practice/demofragment/MainActivity.kt
@@ -2,11 +2,19 @@ package com.practice.demofragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = ArrayListFragment()
+        val fm = supportFragmentManager
+        val ft = fm.beginTransaction()
+
+        ft.add(android.R.id.content, list)
+        ft.commit()
     }
 }
